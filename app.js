8b210e7c824d8894f62976857f3050b348ac3606
majascale <maja.stojanova@scalefocus.com>
2025-02-27 15:02:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,6 @@ app.post("/mcargs", (req, res) => {
   
   //start test for jwt
   console.log("Request Body: " + req.body.toString("utf8"));
-
   require('jsonwebtoken').verify(req.body.toString("utf8"), yoursecret, {
         algorithm: 'HS256'
     }, (err, decoded) => {
@@ -138,7 +137,7 @@ app.post("/mcargs", (req, res) => {
         console.log('Thumbnail Url: ' + thumbnailUrl); 
 
         console.log(typeof type);
-        console.log(type == 'sms;);  
+        console.log(type == 'sms');  
         console.log(type === 'sms'); 
 
     });
